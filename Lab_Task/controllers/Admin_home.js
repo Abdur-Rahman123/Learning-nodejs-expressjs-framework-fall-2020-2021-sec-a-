7ad8e15339f3a79ef6debcd5ae7d48af0981a98e
Abdur-Rahman123 <32038743+Abdur-Rahman123@users.noreply.github.com>
2020-11-16 22:49:53 +0600

    admin inser,delete, all user registration finish

--- Lab_Task/controllers/Admin_home.js
+++ Lab_Task/controllers/Admin_home.js
@@ -10,4 +10,87 @@ router.get('*',  (req, res, next)=>{
 router.get('/', (req, res)=>{
 	res.render('Admin_home/index', {name: req.cookies['uname']});
 });
+router.get('/Gcreate', (req, res)=>{
+	res.render('user/Gcreate');
+});
+
+router.post('/Gcreate', (req, res)=>{
+	var user = {
+		username : req.body.username,
+		password : req.body.password,
+		type : '2'
+	}
+	userModel.insert(user, function(status){
+		if(status){
+			res.redirect('/Admin_home/userlist');
+		}else{
+			res.redirect('/user/create');
+		}
+	});
+});
+router.get('/Screate', (req, res)=>{
+	res.render('user/Screate');
+});
+
+router.post('/Screate', (req, res)=>{
+	var user = {
+		username : req.body.username,
+		password : req.body.password,
+		type : '1'
+	}
+	userModel.insert(user, function(status){
+		if(status){
+			res.redirect('/Admin_home/userlist');
+		}else{
+			res.redirect('/user/create');
+		}
+	});
+});
+router.get('/userlist', (req, res)=>{
+
+	userModel.getAll(function(results){
+		res.render('Admin_home/userlist', {users: results});
+	});
+
+});
+router.get('/delete/:id/:username/:password/:type', (req, res)=>{
+	var user = {
+		id:req.params.id,
+		username:req.params.password,
+		password:req.params.password,
+		type:req.params.type
+	 };
+
+	res.render('user/delete',user);
+});
+
+router.post('/delete/:id/:username/:password/:email', (req, res)=>{
+
+	userModel.delete(req.params.id, function(status){
+		if(status){
+			res.redirect('/Admin_home/userlist');
+		}else{
+			res.redirect('/user/delete');
+		}
+	});
+});
+
+router.get('/Register', (req, res)=>{
+	res.render('Admin_home/Register');
+});
+
+router.post('/Register', (req, res)=>{
+	var user = {
+		username : req.body.username,
+		password : req.body.password,
+		type : req.body.type
+	}
+	userModel.insert(user, function(status){
+		if(status){
+			res.redirect('/Admin_home/userlist');
+		}else{
+			res.redirect('/Admin_home/Register');
+		}
+	});
+});
 module.exports=router;
--- Lab_Task/controllers/Scout_home.js
+++ Lab_Task/controllers/Scout_home.js
@@ -10,4 +10,22 @@ router.get('*',  (req, res, next)=>{
 router.get('/', (req, res)=>{
 	res.render('Scout_home/index', {name: req.cookies['uname']});
 });
+router.get('/Register', (req, res)=>{
+	res.render('Scout_home/Register');
+});
+
+router.post('/Register', (req, res)=>{
+	var user = {
+		username : req.body.username,
+		password : req.body.password,
+		type : req.body.type
+	}
+	userModel.insert(user, function(status){
+		if(status){
+			res.redirect('/Scout_home/injex');
+		}else{
+			res.redirect('/Scout_home/Register');
+		}
+	});
+});
 module.exports=router;
